f1e3978725aaba5f7e291f0e50ec94fec2b0f902
Hai Liu <hailiu@gmail.com>
2018-05-31 10:44:18 -0700

    full width top

--- fuse-ui-fabric/logoHeader/logoHeader.classNames.ts
+++ fuse-ui-fabric/logoHeader/logoHeader.classNames.ts
@@ -2,7 +2,9 @@ import { mergeStyleSets } from '@uifabric/merge-styles';
 
 const logoHeaderClassNames = (): { [key: string]: string } => mergeStyleSets({
   root: {
+    width: '100%',
     display: 'flex',
+    flexShrink: 0,
     justifyContent: 'space-between'
   },
   logo: {
