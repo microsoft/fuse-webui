04f4c1c963916bd76b57879931c611357af8e780
Hai Liu <hailiu@gmail.com>
2018-11-19 12:59:33 -0800

    add missing reducers

---
+++ fuse-react-gen/examples/{{app}}/reducers/authContext.ts
@@ -0,0 +1,8 @@
+import { AdalAuthContext } from '@fuselab/ui-adal';
+import { Action } from '../actions';
+
+function authContext(state: AdalAuthContext = null, action: Action): AdalAuthContext {
+  return state;
+}
+
+export default authContext;
--- fuse-react-gen/examples/{{app}}/reducers/index.ts
+++ fuse-react-gen/examples/{{app}}/reducers/index.ts
@@ -1,8 +1,10 @@
 import { preference, user } from '@fuselab/ui-fabric/reducers';
 import { combineReducers } from 'redux';
 import { Store } from '../store';
+import authContext from './authContext';
 
 export default combineReducers<Store>({
+  authContext,
   user,
   preference
 });
