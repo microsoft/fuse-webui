6c4204dc03f3b84917f76908113bbb8f21cb9843
Mike Bennett <mibenne@microsoft.com>
2019-02-26 15:14:16 -0800

    Added options to disable theme and compact toggle to userProfile, and made dark theme apply to button.

--- fuse-ui-fabric/userProfile/userProfile.classNames.ts
+++ fuse-ui-fabric/userProfile/userProfile.classNames.ts
@@ -6,6 +6,23 @@ const userProfileClassNames = (): { [key: string]: string } => mergeStyleSets({
     display: 'flex',
     alignItems: 'center',
     paddingRight: 4
+  },
+  loginLink: {
+    lineHeight: 32
+  },
+  darkThemeButton: {
+    backgroundColor: 'black',
+    color: 'white',
+    selectors: {
+      ':hover': {
+        backgroundColor: '#575757',
+        color: 'white'
+      },
+      ':active': {
+        backgroundColor: '#373737',
+        color: 'white'
+      }
+    }
   }
 });
 
