415d0568757c92d487e839b98e23a4a5b5416b5c
Hai Liu <hailiu@gmail.com>
2019-03-28 10:40:28 -0700

    function helper with mixin and dispatchActions

---
+++ fuse-ui-fabric/utilities/dispatchActions.ts
@@ -0,0 +1,15 @@
+import { FilterMemberNames, Func, mixin } from '@fuselab/ui-shared/typeHelpers';
+import { Action, Dispatch } from 'redux';
+
+function dispatchActions<T, S>(actionCreators: T, dispatch: Dispatch<S>, ...methods: FilterMemberNames<T, Func<Action>>[]): T {
+  return mixin(
+    actionCreators,
+    (x: Action) => {
+      dispatch(x);
+
+      return x;
+    },
+    ...methods);
+}
+
+export default dispatchActions;
