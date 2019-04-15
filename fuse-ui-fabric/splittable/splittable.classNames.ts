ac242bedc28c0d6b3672ba695c9d695f846df1fc
Hai Liu <hailiu@gmail.com>
2019-04-15 10:20:37 -0700

    fix splitter border appearance

--- fuse-ui-fabric/splittable/splittable.classNames.ts
+++ fuse-ui-fabric/splittable/splittable.classNames.ts
@@ -1,29 +1,51 @@
 import { mergeStyleSets } from '@uifabric/merge-styles';
 
+const classNames = {
+  root: 'splittable-root',
+  divider: 'splittable-divider',
+  part: 'splittable-part'
+};
+
 const splittableClassNames = (orientation: 'vertical' | 'horizontal'): { [key: string]: string } => mergeStyleSets(<any>{
-  root: {
-    display: 'flex',
-    flexWrap: 'no-wrap',
-    flexDirection: orientation === 'vertical' ? 'row' : 'column'
-  },
-  divider: {
-    cursor: orientation === 'vertical' ? 'ew-resize' : 'ns-resize',
-    borderStyle: 'solid',
-    width: orientation === 'vertical' ? 5 : 'auto',
-    height: orientation === 'vertical' ? 'auto' : 5,
-    background: 'lightgray',
-    boxSizing: 'border-box',
-    borderWidth: orientation === 'vertical' ? '0px 2px' : '2px 0px',
-    borderColor: 'white',
-    selectors: {
-      ':hover': {
-        borderColor: 'lightgray'
+  root: [
+    classNames.root,
+    {
+      display: 'flex',
+      flexWrap: 'no-wrap',
+      flexDirection: orientation === 'vertical' ? 'row' : 'column',
+      selectors: {
+        [`.${classNames.part} &`]: {
+          width: orientation === 'vertical' ? 'auto' : '100%',
+          height: orientation === 'vertical' ? '100%' : 'auto'
+        }
       }
-    }
-  },
-  part: {
-    padding: orientation === 'vertical' ? '0 12px' : '12px 0'
-  }
+    }],
+  divider: [
+    classNames.divider,
+    {
+      cursor: orientation === 'vertical' ? 'ew-resize' : 'ns-resize',
+      borderStyle: 'solid',
+      width: orientation === 'vertical' ? 5 : 'auto',
+      height: orientation === 'vertical' ? 'auto' : 5,
+      background: 'lightgray',
+      boxSizing: 'border-box',
+      borderWidth: orientation === 'vertical' ? '0px 2px' : '2px 0px',
+      borderColor: 'white',
+      selectors: {
+        ':hover': {
+          borderColor: 'lightgray'
+        },
+        [`.${classNames.part} & `]: {
+          margin: orientation === 'vertical' ? '-12px 0' : '0 -12px'
+        }
+      }
+    }],
+  part: [
+    classNames.part,
+    {
+      overflow: 'hidden',
+      padding: orientation === 'vertical' ? '0 12px' : '12px 0'
+    }]
 });
 
 export default splittableClassNames;
