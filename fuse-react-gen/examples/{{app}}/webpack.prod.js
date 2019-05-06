0828904547eed43ea660e8d4b4189da6b7b57b07
Hai Liu <hailiu@gmail.com>
2019-05-06 09:43:01 -0700

    user terser for minify (#15)

--- fuse-react-gen/examples/{{app}}/webpack.prod.js
+++ fuse-react-gen/examples/{{app}}/webpack.prod.js
@@ -1,11 +1,12 @@
 const merge = require('webpack-merge');
 const prod = require('@fuselab/build-config/webpack.prod');
+const _ = require('underscore');
 const common = require('./webpack.common');
 const package = require('./package.json');
 const version = `v${package.version}`;
 const appName = package.appName;
 
 module.exports = merge(common,
-  { mode: 'none' },
+  _.omit(prod, 'plugins'),
   { plugins: prod.plugins(appName, version) },
   { output: { filename: '[name].bundle.min.[chunkhash].js' } });;
