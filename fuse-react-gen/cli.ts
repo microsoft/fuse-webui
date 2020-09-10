d3193d088f8f58b397157758ea56854a2b3fcf1d
Hai Liu <hailiu@gmail.com>
2020-09-09 21:33:38 -0700

    fix scriptName

--- fuse-react-gen/cli.ts
+++ fuse-react-gen/cli.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
-import { commandDir } from 'yargs';
+import { commandDir, scriptName } from 'yargs';
 
 //tslint:disable-next-line:no-unused-variable
-const _dummy = commandDir('./commands')
+const _dummy = scriptName('fuse-gen')
+  .commandDir('./commands')
   .help()
   .demandCommand(1).argv;
